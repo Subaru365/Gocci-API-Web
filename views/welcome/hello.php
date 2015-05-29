48da16d5ae8727682006eb87e5c811187546e410
vagrant <vagrant@localhost.localdomain>
2015-05-29 04:54:49 -0400

    .composerをアップデート

--- views/welcome/hello.php
+++ views/welcome/hello.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+sa<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
@@ -40,7 +40,7 @@
 			<div class="col-md-12">
 				<h1>Hello, <?php echo $name; ?>! <small>Congratulations, you just used a Presenter!</small></h1>
 				<hr>
-				<p>The controller generating this page is found at <code>APPPATH/classes/controller/welcome.php</code>.</p>
+				<p>The varcontroller generating this page is found at <code>APPPATH/classes/controller/welcome.php</code>.</p>
 				<p>This view is located at <code>APPPATH/views/welcome/hello.php</code>.</p>
 				<p>It is loaded via a Presenter class with a name of <code>Presenter_Welcome_Hello</code>, located in <code>APPPATH/classes/presenter/welcome/hello.php</code></p>
 			</div>
