962c9c488d51f5e1b766494ecd619ebd72c5c0ab
root <root@localhost.localdomain>
2015-06-01 05:38:23 +0000

    add: restpage branch all commits

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -1,10 +1,15 @@
 <?php
 /**
- * Use this file to override global defaults.
- *
- * See the individual environment DB configs for specific config information.
+ * The development database settings. These get merged with the global settings.
  */
 
 return array(
-
+	'default' => array(
+		'connection'  => array(
+			'dsn'        => 'mysql:host=127.0.0.1;dbname=gocci',
+			'username'   => 'root',
+			'password'   => '',
+		),
+		'charset'	=> 'utf8',
+	),
 );
