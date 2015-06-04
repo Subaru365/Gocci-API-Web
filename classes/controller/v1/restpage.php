d46b17d149217196a0db1a7f73cee5cf094b5791
Subaru365 <subaru.woody@gmail.com>
2015-06-03 21:22:13 -0400

    店舗ページrest.phpを実装
    オブジェクト化未対応

--- classes/controller/v1/restpage.php
+++ classes/controller/v1/restpage.php
@@ -13,24 +13,26 @@ class Controller_V1_Restpage extends Controller_Rest
 
     public function action_index()
     {
-	$limit = Input::get('limit');
-	if (empty($limit)) {
-	    $limit = 30;
-	}
-        $restname = Input::get('restname');
-	if (!empty($restname)) {
-	    $query = DB::query('SELECT * FROM users');
-	    //$result = $query->execute()->as_array();
-	    $result = Model_Tag::find_all();	
-            /* 今はサンプルとしてやっているが、本来は
-	       $result = Model_Restpage::find_all();
-	       のようにしてModelから取得してくるようにする
-	    */	
+		$limit = Input::get('limit');
 
-	    $rows = array("header" => $result);
-	    $this->response($rows);
-	    //echo json_encode( array( 1=>array( 'a' => '6' ),  'a'=>'x'  ) );
-	    //ob_get_clean();
-	}
+		if (empty($limit)) {
+	    	$limit = 30;
+		}
+
+        $restname = Input::get('restname');
+		if (!empty($restname)) {
+	    	$query = DB::query('SELECT * FROM users');
+	    	//$result = $query->execute()->as_array();
+	    	$result = Model_Tag::find_all();
+            /* 今はサンプルとしてやっているが、本来は
+	       　$result = Model_Restpage::find_all();
+	       　のようにしてModelから取得してくるようにする
+	    	*/
+
+	    	$rows = array("header" => $result);
+	    	$this->response($rows);
+	    	//echo json_encode( array( 1=>array( 'a' => '6' ),  'a'=>'x'  ) );
+	    	//ob_get_clean();
+		}
     }
 }
--- classes/model/post.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Model_Post extends Model_Crud
-{
-     protected static $_table_name = 'posts';
-}
