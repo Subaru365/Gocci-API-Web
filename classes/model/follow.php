0598958983864a3d3a0775eda56eb56c9f7dcead
Subaru365 <subaru.woody@gmail.com>
2015-06-10 21:32:44 -0400

    follow_flagをモデル化
    	modified:   fuel/app/classes/model/follow.php

--- classes/model/follow.php
+++ classes/model/follow.php
@@ -1,5 +1,22 @@
 <?php
-class Model_Follow extends Model_Crud
+class Model_Follow extends Model
 {
-     protected static $_table_name = 'follows';
+	public static function get_flag($user_id, $post_user_id)
+	{
+		//クエリ文
+		$query = DB::select('follow_id')->from('follows');
+		$query->where 	  ('follow_a_user_id', "$user_id");
+		$query->and_where ('follow_p_user_id', "$post_user_id")
+
+
+		$result = $query->execute()->as_array();
+
+		if ($result == true) {
+			$follow_flag = 1;
+		}else{
+			$follow_flag = 0;
+		}
+
+		return $follow_flag;
+	}
 }
