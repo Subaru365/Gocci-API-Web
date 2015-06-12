5c3f1d0f2349ca00cd75bf7a4a245986cb64d37e
Subaru365 <subaru.woody@gmail.com>
2015-06-12 14:59:04 -0400

    Controller_Timeline DateTime書式変更完了
    	modified:   fuel/app/classes/controller/v1/timeline.php

--- fuel/app/classes/controller/v1/timeline.php
+++ fuel/app/classes/controller/v1/timeline.php
@@ -35,7 +35,7 @@ class Controller_V1_Timeline extends Controller
 				$post_id	  = $post_data[$i]['post_id'];
 				$post_user_id = $post_data[$i]['post_user_id'];
 				$post_rest_id = $post_data[$i]['post_rest_id'];
-
+				$post_date    = $post_data[$i]['post_date'];
 
 	    		$like_num 	  = Model_Like::get_num($post_id);
 	    		$post_data[$i]['like_num']    = $like_num;
@@ -51,6 +51,37 @@ class Controller_V1_Timeline extends Controller
 
 	    		$like_flag	  = Model_Like::get_flag($user_id, $post_id);
 	    		$post_data[$i]['like_flag']   = $like_flag;
+
+
+	    		$datetime1 = new DateTime("$post_date");
+				$datetime2 = new DateTime(date('Y-m-d H:i:s'));
+
+				$interval = $datetime1->diff($datetime2);
+
+				if ($interval->format('%y') > 0) {
+					$date_diff = $interval->format('%y') . '年前';
+
+				}elseif ($interval->format('%m') > 0) {
+					$date_diff = $interval->format('%m') . 'ヶ月前';
+
+				}elseif ($interval->format('%d') > 0) {
+					$date_diff = $interval->format('%d') . '日前';
+
+				}elseif ($interval->format('%h') > 0) {
+					$date_diff = $interval->format('%h') . '時間前';
+
+				}elseif ($interval->format('%i') > 0) {
+					$date_diff = $interval->format('%i') . '分前';
+
+				}elseif ($interval->format('%s') > 0) {
+					$date_diff = $interval->format('%s') . '秒前';
+
+				}else{
+					$date_diff = '未来から';
+					error_log('$post_dateの時刻エラー');
+				}
+
+				$post_data[$i]['post_date'] = $date_diff;
 
 			}
 
