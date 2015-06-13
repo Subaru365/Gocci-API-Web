b0a63fcfec5a4c575d60c495065594ea8de99a71
Subaru365 <subaru.woody@gmail.com>
2015-06-12 23:35:02 -0400

    テスト：Model_Post $post_dataリファクタリング完了
    	modified:   fuel/app/classes/controller/v1/test.php

--- classes/controller/v1/test.php
+++ classes/controller/v1/test.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type: application/json; charset=UTF-8');
 error_reporting(-1);
 
 class Controller_V1_Test extends Controller
@@ -6,58 +7,22 @@ class Controller_V1_Test extends Controller
 
 	public function action_index()
 	{
-		$limit = 30;
-		$sort_key  = 'all';
-		$post_data = Model_Post::get_data($sort_key, $sort_key, $limit);
-		$post_num  = count($post_data);
+
+	$rest_id = Input::get('rest_id');
+    $user_id = Input::get('user_id');
+	$limit   = Input::get('limit');
+
+		$sort_key  = 'rest';
+		$post_data = Model_Post::get_data($user_id, $sort_key, $rest_id, $limit);
+
+		$data = array(
+	    		"posts" => $post_data
+	    	);
+
+		$restaurantpage = json_encode($data , JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES );
 
 
-		for ($i=0; $i < $post_num; $i++) {
-			$post_date[$i] = $post_data[$i]['post_date'];
-
-			$datetime1 = new DateTime("$post_date[$i]");
-			$datetime2 = new DateTime(date('Y-m-d H:i:s'));
-
-			$interval = $datetime1->diff($datetime2);
-
-
-			print_r("$post_date[$i]" . "\n");
-			echo $interval->format('%y, %m, %d, %h, %i, %s');
-
-
-			$datetime1 = new DateTime("$post_date[$i]");
-			$datetime2 = new DateTime(date('Y-m-d H:i:s'));
-
-			$interval = $datetime1->diff($datetime2);
-
-			if ($interval->format('%y') > 0) {
-				$date_diff = $interval->format('%y') . '年前';
-
-			}elseif ($interval->format('%m') > 0) {
-				$date_diff = $interval->format('%m') . 'ヶ月前';
-
-			}elseif ($interval->format('%d') > 0) {
-				$date_diff = $interval->format('%d') . '日前';
-
-			}elseif ($interval->format('%h') > 0) {
-				$date_diff = $interval->format('%h') . '時間前';
-
-			}elseif ($interval->format('%i') > 0) {
-				$date_diff = $interval->format('%i') . '分前';
-
-			}elseif ($interval->format('%s') > 0) {
-				$date_diff = $interval->format('%s') . '秒前';
-
-			}else{
-				$date_diff = '未来から';
-				error_log('$post_dateの時刻エラー');
-			}
-
-			echo "\n" . "$date_diff" . "\n";
-
-		}
-
-
+	    echo "$restaurantpage";
 
 
 
