ecd45b45de474e7f411816c5c352431f7ccf2c1c
Subaru365 <subaru.woody@gmail.com>
2015-06-14 02:38:39 -0400

    [開始]　Controller_Auth ログイン認証API
    	new file:   fuel/app/classes/controller/v1/auth.php

---
+++ fuel/app/classes/controller/v1/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+header('Content-Type: application/json; charset=UTF-8');
+error_reporting(-1);
+/**
+ * Pupular api
+ *
+ */
+
+class Controller_V1_Auth extends Controller
+{
+
+    public function action_index()
+    {
+
+
+
+	}
+}
