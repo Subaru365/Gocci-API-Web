1f3f5b62160ba98e209614e300041785f807a686
Subaru365 <subaru.woody@gmail.com>
2015-06-15 01:18:47 -0400

    [開始] Controller_Notice 通知APIの実装
    	modified:   fuel/app/classes/controller/v1/notice.php

---
+++ fuel/app/classes/controller/v1/notice.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/json; charset=UTF-8');
+error_reporting(-1);
+/**
+ * notice api
+ *
+ */
+
+class Controller_V1_Notice extends Controller
+{
+
+    public function action_index()
+    {
+    	$user_id = Input::get('user_id');
+
+
+
+	}
+}
