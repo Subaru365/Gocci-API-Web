4531576a81c139ee956491402bcf51ec967bb746
Subaru365 <subaru.woody@gmail.com>
2015-06-15 07:35:31 -0400

    Model_Dateを各モデルに対応
    Model_(Notice, Comment, Post)
    	modified:   fuel/app/classes/model/comment.php
    	modified:   fuel/app/classes/model/notice.php
    	modified:   fuel/app/classes/model/post.php

--- classes/model/post.php
+++ classes/model/post.php
@@ -84,40 +84,6 @@ class Model_Post extends Model
 	    	$date_diff 	  = Model_Date::get_data($post_date);
 			$post_data[$i]['post_date']   = $date_diff;
 
-
-	    	/*
-
-	    	//post_date表示形式 変換
-
-	    	$datetime1 = new DateTime("$post_date");
-			$datetime2 = new DateTime(date('Y-m-d H:i:s'));
-
-			$interval = $datetime1->diff($datetime2);
-
-			if ($interval->format('%y') > 0) {
-				$date_diff = $interval->format('%y') . '年前';
-
-			}elseif ($interval->format('%m') > 0) {
-				$date_diff = $interval->format('%m') . 'ヶ月前';
-
-			}elseif ($interval->format('%d') > 0) {
-				$date_diff = $interval->format('%d') . '日前';
-
-			}elseif ($interval->format('%h') > 0) {
-				$date_diff = $interval->format('%h') . '時間前';
-
-			}elseif ($interval->format('%i') > 0) {
-				$date_diff = $interval->format('%i') . '分前';
-
-			}elseif ($interval->format('%s') > 0) {
-				$date_diff = $interval->format('%s') . '秒前';
-
-			}else{
-				$date_diff = '未来から';
-				error_log('$post_dateの時刻エラー');
-			}
-			*/
-
 		}
 
 		return $post_data;
