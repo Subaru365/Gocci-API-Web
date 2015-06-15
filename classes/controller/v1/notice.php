810b2bb608f829877914f9087575674ad4bfaa1a
Subaru365 <subaru.woody@gmail.com>
2015-06-15 07:10:19 -0400

    Model_Dateの作成 $date_diff格納をモデル化
    引数はTIMESTAMP情報。
    	new file:   fuel/app/classes/model/date.php

--- classes/controller/v1/notice.php
+++ classes/controller/v1/notice.php
@@ -13,7 +13,10 @@ class Controller_V1_Notice extends Controller
     {
     	$user_id = Input::get('user_id');
 
+    	$data = Model_Notice::get_data($user_id);
 
+    	$notice = json_encode($data , JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES );
 
+	   	echo "$notice";
 	}
 }
--- classes/model/notice.php
+++ classes/model/notice.php
@@ -2,4 +2,27 @@
 class Model_Notice extends Model_Crud
 {
      protected static $_table_name = 'notices';
+
+     public static function get_data($user_id)
+     {
+     	$query = DB::select(
+			'notices.notice_id', 'notices.notice_a_user_id', 'users.username',
+			'users.profile_img', 'notices.notice', 'notices.notice_post_id',
+			'notices.read_flag', 'notices.notice_date')
+
+     	->from('notices')
+
+		->join('users', 'INNER')
+		->on('notices.notice_a_user_id', '=', 'users.user_id')
+
+		->order_by('notices.notice_date','desc')
+
+		->limit('15')
+
+		->where('notices.notice_p_user_id', "$user_id");
+
+		$notice_data = $query->execute()->as_array();
+
+		return $notice_data;
+     }
 }
