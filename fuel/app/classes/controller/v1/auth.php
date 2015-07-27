1780ebbe14420e8d1873d0763da8132a5d1ab84e
Subaru365 <subaru.woody@gmail.com>
2015-07-27 23:29:06 +0900

    Refactor code a bit to avoid bug.

--- fuel/app/classes/controller/v1/auth.php
+++ fuel/app/classes/controller/v1/auth.php
@@ -1,19 +1,20 @@
 <?php
 
-header('Content-Type: application/json; charset=UTF-8');
+// header('Content-Type: application/json; charset=UTF-8');
 error_reporting(-1);
 /**
  * Auth api
  *
- *         //$time_start = microtime(true);
- *  //debug
- *       //$timelimit = microtime(true) - $time_start;
- *       //echo '格納完了：' . $timelimit . ' seconds\r\n';
+ * // $time_start = microtime(true);
+ * // debug
+ * // $timelimit = microtime(true) - $time_start;
+ * // echo '格納完了：' . $timelimit . ' seconds\r\n';
+ *
  */
 
 class Controller_V1_Auth extends Controller
 {
-
+/*
     //SNSサインイン
     public function action_sns()
     {
@@ -30,7 +31,7 @@ class Controller_V1_Auth extends Controller
         $username    = $user_data['Records'][3]['Value'];
 
 
-        $user_id     = Model_User::check_id($identity_id);
+        //$user_id     = Model_User::check_id($identity_id);
 
 
         //初回サインイン
@@ -38,7 +39,7 @@ class Controller_V1_Auth extends Controller
 
             $user_id = Model_User::get_next_id();
 
-            $status = $this->signup(
+            self::signup(
                 $keyword, $user_id, $username, $profile_img,
                 $os, $model, $register_id, $identity_id);
 
@@ -69,241 +70,282 @@ class Controller_V1_Auth extends Controller
 
             $badge_num = Model_User::get_badge($user_id);
 
-            $status = $this->success(
+            self::success(
                 $keyword, $user_id, $username,
                 $profile_img, $identity_id, $badge_num, $token);
         }
     }
+*/
 
-
-    //Guestサインイン
-    public function action_guest()
+    // サインアップ
+    public function action_signup()
     {
-        $keyword     = 'Guest';
-        $profile_img = 'none';
+        $keyword     = 'サインアップ';
         $user_id     = Model_User::get_next_id();
-
         $username    = Input::get('username');
         $os          = Input::get('os');
         $model       = Input::get('model');
         $register_id = Input::get('register_id');
 
+        Model_User::check_name($username);
+        Model_Device::check_register_id($register_id);
 
-        $device_data = Model_Device::check_device($register_id);
-
-        if (empty($check_device)) {
-
-
-        }else{
-        //既存登録端末あり
-
-            $user_id_old = $device_data[0]['device_user_id'];
-            $endpoint_arn= $device_data[0]['endpoint_arn'];
-
-            $tmp = Model_Sns::delete_endpoint($endpoint_arn);
-            $tmp = Model_Device::delete_device($user_id_old);
-        }
-
-
-        $cognito_data = Model_Cognito::post_data(
-            $user_id, $username, $os, $model, $register_id);
-
-        $identity_id = $cognito_data['IdentityId'];
-        $token       = $cognito_data['Token'];
-
-        $status = $this->signup(
-            $keyword, $user_id, $username, $profile_img,
-            $os, $model, $register_id, $identity_id, $token);
+        self::signup(
+            $keyword, 
+            $user_id, 
+            $username, 
+            $os, 
+            $model, 
+            $register_id);
     }
 
-
-    //ログイン
-    public function action_welcome()
+    // ログイン
+    public function action_login()
     {
-        $keyword     = 'セッション';
-        $token       = 'none';
+        $keyword     = 'ログイン';
         $identity_id = Input::get('identity_id');
-        $sns_flag    = Input::get('sns_flag');
 
         $user_data   = Model_User::get_auth($identity_id);
-
         $user_id     = $user_data['user_id'];
         $username    = $user_data['username'];
         $profile_img = $user_data['profile_img'];
         $badge_num   = $user_data['badge_num'];
 
-        if ($sns_flag == 0) {
-            $token = Model_Cognito::get_token($user_id, $identity_id);
+        // identity_idがgetされていない場合
+        if (empty($identity_id))
+        { 
+            $data = [
+                'code'    => 400,
+                'message' => 'identity_idが空です'
+            ];
+
+            Controller_V1_Base::output_json($data);
+            exit; 
         }
 
-        $login = Model_Login::post_login($user_id);
+        // ここでエラー発生。        
+        $token = Model_Cognito::get_token($user_id, $identity_id);
+        // $token = '';
+        
+        Model_Login::post_login($user_id);
 
-        $status = $this->success(
-            $keyword, $user_id, $username,
-            $profile_img, $identity_id, $badge_num, $token);
+        self::success(
+            $keyword, 
+            $user_id, 
+            $username,
+            $profile_img, 
+            $identity_id, 
+            $badge_num, 
+            $token);
     }
 
 
-    //初回データ格納関数 (RDS, SNS)
+    // 初回データ格納 (RDS, SNS)
     private static function signup(
-        $keyword, $user_id, $username, $profile_img,
-        $os, $model, $register_id, $identity_id, $token = 'none')
-    {
+        $keyword, 
+        $user_id, 
+        $username, 
+        $os, 
+        $model, 
+        $register_id
+    ) {
         $badge_num = 0;
 
         try
         {
-            $profile_img = Model_User::post_data(
-                $username, $profile_img, $identity_id);
+            $cognito_data = Model_Cognito::post_data(
+                            $user_id, 
+                            $username, 
+                            $os, 
+                            $model, 
+                            $register_id);
 
+            $identity_id  = $cognito_data['IdentityId'];
+            $token        = $cognito_data['Token'];
+
+            $profile_img  = Model_User::post_data($username, $identity_id);
 
             $endpoint_arn = Model_Sns::post_endpoint(
                 $user_id, $identity_id, $register_id, $os);
 
-
-            //Device情報を登録
-            $device = Model_Device::post_data(
+            Model_Device::post_data(
                 $user_id, $os, $model, $register_id, $endpoint_arn);
 
-            //success出力へ
-            $status = $this->success(
-                $keyword, $user_id, $username,
-                $profile_img, $identity_id, $badge_num, $token);
+            // success出力へ
+            self::success(
+                $keyword, 
+                $user_id, 
+                $username,
+                $profile_img, 
+                $identity_id, 
+                $badge_num, 
+                $token);
         }
 
-
-        //データベース登録エラー
+        // データベース登録エラー
         catch(\Database_Exception $e)
         {
-            //failed出力へ
-            $status = $this->failed(
-                $keyword, $user_id, $username,
-                $profile_img, $identity_id, $badge_num, $token);
+            // failed出力
+            self::failed(
+                $keyword, 
+                $user_id, 
+                $username,
+                $profile_img, 
+                $identity_id, 
+                $badge_num, 
+                $token);
 
             error_log($e);
         }
     }
 
-
-    //DBデータ入力成功
-    private function success(
-        $keyword, $user_id, $username,
-        $profile_img, $identity_id, $badge_num, $token)
-    {
-        $data = array(
+    // DBデータ入力成功
+    private static function success(
+        $keyword, 
+        $user_id, 
+        $username,
+        $profile_img, 
+        $identity_id, 
+        $badge_num, 
+        $token
+    ) {
+        $data = [
             'code'        => 200,
+            'message'     => "$keyword" . 'しました。',
             'user_id'     => "$user_id",
             'username'    => "$username",
             'profile_img' => "$profile_img",
             'identity_id' => "$identity_id",
             'badge_num'   => "$badge_num",
-            'message'     => "$keyword" . 'でログインしました。',
             'token'       => "$token"
-        );
+        ];
 
-        $status = $this->output_json($data);
+        Controller_V1_Base::output_json($data);
         session::set('user_id', $user_id);
     }
 
 
-    //DBデータ入力エラー
-    private function failed(
-        $keyword, $user_id, $username,
-        $profile_img, $identity_id, $badge_num, $token)
-    {
-        $data = array(
+    // DBデータ入力エラー
+    private static function failed(
+        $keyword, 
+        $user_id, 
+        $username,
+        $profile_img, 
+        $identity_id, 
+        $badge_num, 
+        $token
+    ) {
+        $data = [
             'code'        => 401,
+            'message'     => "$keyword" . 'できませんでした。',
             'username'    => "$username",
             'profile_img' => "$profile_img",
             'identity_id' => "$identity_id",
             'badge_num'   => "$badge_num",
-            'message'     => "$keyword" . 'でログインできませんでした。',
             'token'       => "$token"
-        );
+        ];
 
-        $status = $this->output_json($data);
+        Controller_V1_Base::output_json($data);
     }
 
 
-    private static function output_json($data)
-    {
-        $json = json_encode(
-            $data,
-            JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES
-        );
-
-        echo "$json";
-    }
-
-
-
-    //Conversion
-    //==========================================================================//
+    // Conversion
+    // ========================================================================== //
 
     public function action_conversion()
     {
         $keyword     = '顧客様';
-
         $username    = Input::get('username');
         $profile_img = Input::get('profile_img');
-
         $os          = Input::get('os');
         $model       = Input::get('model');
         $register_id = Input::get('register_id');
 
         $user_id     = Model_User::check_conversion($username);
 
-
-        //初期化ユーザー
+        // 初期化ユーザー
         if (empty($user_id)) {
-            $user_id     = Model_User::get_next_id();
 
-            //IdentityID取得
-            $identity_id = Model_Cognito::post_data(
-                $user_id, $username, $os, $model, $register_id);
+            $user_id      = Model_User::get_next_id();
+            $cognito_data = Model_Cognito::post_data(
+            $user_id, $username, $os, $model, $register_id);
 
-            $status = $this->signup(
-                $keyword, $user_id, $username, $profile_img,
-                $os, $model, $register_id, $identity_id);
+            $identity_id  = $cognito_data['IdentityId'];
+            $token        = $cognito_data['Token'];
+
+            $profile_img  = Model_S3::input($user_id, $profile_img);
+            $profile_img  = Model_User::post_conversion(
+                $username, $profile_img, $identity_id);
+
+            $endpoint_arn = Model_Sns::post_endpoint(
+                $user_id, $identity_id, $register_id, $os);
+
+            Model_Device::post_data(
+                $user_id, $os, $model, $register_id, $endpoint_arn);
+
+            self::success(
+                $keyword, $user_id, $username,
+                $profile_img, $identity_id, $badge_num, $token);
 
 
-        //VIPユーザー
-        }else{
-            $user_id = $user_id[0]['user_id'];
+        // VIPユーザー
+        } else {
+            $user_id      = $user_id[0]['user_id'];
 
-            //IdentityID取得
-            $identity_id = Model_Cognito::post_data(
-                $user_id, $username, $os, $model, $register_id);
+            // IdentityID取得
+            $cognito_data = Model_Cognito::post_data(
+                $user_id, 
+                $username, 
+                $os, 
+                $model, 
+                $register_id);
 
+            $identity_id  = $cognito_data['IdentityId'];
+            $token        = $cognito_data['Token'];
 
             try{
+                $profile_img  = Model_S3::input($user_id, $profile_img);
 
-                $user_data = Model_User::update_data(
-                    $user_id, $username, $profile_img, $identity_id);
+                $user_data    = Model_User::update_data(
+                    $user_id, 
+                    $username, 
+                    $profile_img, 
+                    $identity_id);
 
                 $endpoint_arn = Model_Sns::post_endpoint(
-                    $user_id, $identity_id, $register_id);
+                    $user_id, 
+                    $identity_id, 
+                    $register_id);
 
-                //Device情報を登録
+                // Device情報を登録
                 $device = Model_Device::update_data(
-                    $user_id, $os, $model, $register_id, $endpoint_arn);
+                    $user_id, 
+                    $os, 
+                    $model, 
+                    $register_id, 
+                    $endpoint_arn);
 
-                //success出力へ
-                $status = $this->success(
-                    $keyword, $user_id, $username,
-                    $profile_img, $identity_id, $badge_num);
+                // success出力へ
+                self::success(
+                    $keyword, 
+                    $user_id, 
+                    $username,
+                    $profile_img, 
+                    $identity_id, 
+                    $badge_num);
             }
 
-            //データベース登録エラー
+            // データベース登録エラー
             catch(\Database_Exception $e)
             {
-                //failed出力へ
-                $status = $this->failed(
-                    $keyword, $username, $profile_img, $identity_id, $badge_num);
+                self::failed(
+                    $keyword, 
+                    $username, 
+                    $profile_img, 
+                    $identity_id, 
+                    $badge_num);
                 error_log($e);
             }
         }
     }
-
-
 }
