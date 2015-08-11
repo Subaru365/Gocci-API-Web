e1c8a2c739730821fbf031a9b4e16cac68657570
ta_kazu <subaru.woody@gmail.com>
2015-08-11 11:53:08 +0900

    fixed&user test

--- classes/controller/v1/web/auth.php
+++ classes/controller/v1/web/auth.php
@@ -2,6 +2,7 @@
 
 header('Content-Type: application/json; charset=UTF-8');
 error_reporting(-1);
+
 /**
  * Auth api
  *
@@ -9,74 +10,6 @@ error_reporting(-1);
 
 class Controller_V1_Web_Auth extends Controller
 {
-
-
-
-    // // サインアップ
-    // public function action_signup()
-    // {
-    //     $keyword     = 'サインアップ';
-    //     $badge_num   = 0;
-    //     $user_id     = Model_User::get_next_id();
-    //     $username    = Input::get('username');
-    //     $os          = Input::get('os');
-    //     $model       = Input::get('model');
-    //     $register_id = Input::get('register_id');
-
-    //     try
-    //     {
-    //         Model_User::check_name($username);
-    //         Model_Device::check_register_id($register_id);
-
-    //         $cognito_data = Model_Cognito::post_data(
-    //             $user_id,
-    //             $username,
-    //             $os,
-    //             $model,
-    //             $register_id
-    //         );
-
-    //         $identity_id  = $cognito_data['IdentityId'];
-    //         $token        = $cognito_data['Token'];
-
-    //         $profile_img  = Model_User::post_data($username, $identity_id);
-
-    //         $endpoint_arn = Model_Sns::post_endpoint(
-    //             $user_id, $identity_id, $register_id, $os);
-
-    //         Model_Device::post_data(
-    //             $user_id, $os, $model, $register_id, $endpoint_arn);
-
-    //         self::success(
-    //             $keyword,
-    //             $user_id,
-    //             $username,
-    //             $profile_img,
-    //             $identity_id,
-    //             $badge_num,
-    //             $token
-    //         );
-    //     }
-
-    //     // データベース登録エラー
-    //     catch(\Database_Exception $e)
-    //     {
-    //         self::failed(
-    //             $keyword,
-    //             $user_id,
-    //             $username,
-    //             $profile_img,
-    //             $identity_id,
-    //             $badge_num,
-    //             $token
-    //         );
-
-    //         error_log($e);
-    //     }
-    // }
-
-
-
     // ログイン
     public function action_login()
     {
