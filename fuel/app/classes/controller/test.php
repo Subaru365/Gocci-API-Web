b682aeeb0d8ae1e8745c1211b8b70eb23a87cc59
Subaru365 <subaru.woody@gmail.com>
2015-08-13 16:08:47 +0900

    [enhance] Post:Publish
    Change post_status_flug 0 to 1
    
    	modified:   fuel/app/classes/controller/v1/mobile/post.php
    	modified:   fuel/app/classes/model/cognito.php
    	modified:   fuel/app/classes/model/post.php

--- fuel/app/classes/controller/test.php
+++ fuel/app/classes/controller/test.php
@@ -19,41 +19,21 @@ class Controller_Test extends Controller
 		}
 
 		// APIを使用するURLを生成
-		$url = 'http://search.olp.yahooapis.jp/OpenLocalPlatform/V1/localSearch?'
-		  . 'appid=dj0zaiZpPWdpaGw5dkR4WjFINCZzPWNvbnN1bWVyc2VjcmV0Jng9Y2Q-'
-		  . '&lat='."$lat"
-		  . '&lon='."$lon"
-		  . '&dist=0.1' // 上記指定座標からの検索範囲（km）
-		  . '&category=address' // 地名のみ検索
-		  . '&sort=gio';
-		  //. '&n=1'; // 検索結果は1つだけ取得
-		  //. '&datum=wgs' // 世界測地系を使う
-		  //. '&al=3'; // 丁目、字レベルまで取得
+		$url = 'http://geocode.didit.jp/reverse/?'
+		  . 'lat='."$lat"
+		  . '&lon='."$lon";
 
 		// 結果を取得
 		$contents = file_get_contents($url);
 		echo $contents;
 
 		// 住所部分を取得してecho
-		$xml = simplexml_load_string($contents);
+		//$xml = simplexml_load_string($contents);
 		//echo $xml;
 
-		$address = (string)$xml->Item->Address;
+		//$address = (string)$xml->Item->Address;
 		//echo $address;
 		//exit;
 
-	}
-
-	public function action_publish()
-	{
-		$client = new SnsClient([
-			'region'  => 'ap-northeast-1',
-    		'version' => '2010-03-31'
-		]);
-
-		$result = $client->publish([
-    		'Message'   => '{"message":"コメント"}',
-    		'TargetArn' => 'arn:aws:sns:ap-northeast-1:318228258127:endpoint/GCM/gocci-android/fd741ca4-05b6-377c-a608-5ca0f24a6131',
-		]);
 	}
 }
