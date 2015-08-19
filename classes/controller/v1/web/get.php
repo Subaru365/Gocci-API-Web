df7c24ab963dc60a5346f6dae01a31050d0b9282
Subaru365 <subaru.woody@gmail.com>
2015-08-19 18:04:30 +0900

    [merge] Production Confrict

--- classes/controller/v1/web/get.php
+++ classes/controller/v1/web/get.php
@@ -7,11 +7,14 @@ class Controller_V1_Web_Get extends Controller_V1_Web_Base
 	public function action_timeline()
     {
     	// Timeline 
+    	
 	    $sort_key = 'all';
 	    $limit    = 10;
 	    $user_id  = session::get('user_id');
+
 	    $data     = Model_Post::get_data($user_id, $sort_key, $sort_key,$limit);
 	   	$status   = $this->output_json($data);
+	   	
     }
 
 }
