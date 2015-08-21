8b6fbd3fcba58121553e7e49a620ef3819235ee4
OpsWorks user kazu <kazu@onibi.localdomain>
2015-08-21 16:33:13 +0900

    add: get.php

--- fuel/app/classes/controller/v1/web/base.php
+++ fuel/app/classes/controller/v1/web/base.php
@@ -8,7 +8,6 @@ class Controller_V1_Web_Base extends Controller
 {
 	// jwt check
 	
-	/*
 	public function before()
 	{
 		$jwt = @$_SERVER["HTTP_AUTHORIZATION"] ?  @$_SERVER["HTTP_AUTHORIZATION"] : "";
@@ -31,7 +30,7 @@ class Controller_V1_Web_Base extends Controller
 			exit;
 		}
 	}
-	*/
+	
 
 	// decode
     public static function decode($jwt)
@@ -88,7 +87,7 @@ class Controller_V1_Web_Base extends Controller
 			JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES
 		);
 
-		echo "$json";
+		echo $json;
 	}
 
 	public static function getallheaders()
