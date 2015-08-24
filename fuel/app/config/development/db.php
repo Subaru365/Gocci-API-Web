d5303ee5f7b279acc30a34c9050fa785a148c49a
ta_kazu <subaru.woody@gmail.com>
2015-08-24 08:38:05 +0000

    [merge] conflict

---
+++ fuel/app/config/development/db.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The development database settings. These get merged with the global settings.
+ */
+
+return array(
+	'default' => array(
+    	'connection' => array(
+    		/*
+        	'dsn' => 'mysql:host=gocci-master.cywycqfdnnfs.ap-northeast-1.rds.amazonaws.com;dbname=gocci',
+        	'username' => 'inase_gocci',
+        	'password' => 'jtg4lZwUittW8Gfr',
+        	*/
+
+        	'dsn' => 'mysql:host=gocci-test.cywycqfdnnfs.ap-northeast-1.rds.amazonaws.com;dbname=gocci',
+        	'username' => 'inase_gocci_test',
+        	'password' => 'q9Tp2FvG',
+		),
+	),
+);
+
