f861d82d42f947a3874233c794b03a03692ae187
Subaru365 <subaru.woody@gmail.com>
2015-08-27 15:28:14 +0900

    [enhance] Get:timeline, popular, followline [fix] confrict username bag [fix] something...

--- fuel/app/classes/controller/test.php
+++ fuel/app/classes/controller/test.php
@@ -11,13 +11,12 @@ class Controller_Test extends Controller
 	//Followline
 	public function action_index()
 	{
-		$sort_key  = 'follow';
-        $user_id   = Input::get('user_id');
+		$user_id = Input::get('user_id');
 
-		$follow_id = Model_Follow::get_follow_id($user_id);
-		$data = Model_Post::get_data($user_id, $sort_key, $follow_id);
+		$num = count($user_id);
 
-	   	$status = $this->output_json($data);
+		print_r($user_id);
+
 	}
 
 }
