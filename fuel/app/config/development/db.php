68a94ff3286b6e4aad9f27d5a868dcb0082c4aa6
Subaru365 <subaru.woody@gmail.com>
2015-08-27 18:10:04 +0900

    [fix] Get:Popularline

---
+++ fuel/app/config/development/db.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The development database settings. These get merged with the global settings.
+ */
+
+return array(
+	'default' => array(
+    	'connection' => array(
+        	'dsn' => 'mysql:host=gocci-master.cywycqfdnnfs.ap-northeast-1.rds.amazonaws.com;dbname=gocci',
+        	'username' => 'inase_gocci',
+        	'password' => 'jtg4lZwUittW8Gfr',
+		),
+	),
+);
