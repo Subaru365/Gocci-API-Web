d2347ab6a09d12061fdba6719765edd6b2b75d2d
Subaru365 <subaru.woody@gmail.com>
2015-09-29 12:52:47 +0900

    [refresh] iOS-Dev Push Arn
    	modified:   fuel/app/classes/controller/test.php
    	modified:   fuel/app/config/test/_custom.php

--- classes/controller/test.php
+++ classes/controller/test.php
@@ -9,9 +9,9 @@ class Controller_Test extends Controller
 {
 	public static function action_index()
 	{
-		$result	= Model_V2_Db_User::get_user_id_last();
-		$result ++;
-
-        return $result;
+		$identity_id 	= Input::get('identity_id');
+		$user_data		= Model_User::get_auth($identity_id);
+		$user_id 		= $user_data['user_id'];
+		Model_Sns::post_message('Test', 1, $user_id);
 	}
 }
