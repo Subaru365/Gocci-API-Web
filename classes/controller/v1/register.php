b7612d38d94db9a3131f57856281dd0141f28906
Subaru365 <subaru.woody@gmail.com>
2015-12-24 08:49:52 +0000

    add: twitter sns

--- classes/controller/v1/register.php
+++ classes/controller/v1/register.php
@@ -11,6 +11,15 @@
 
 class Controller_V1_Register extends Controller_V1_Base
 {
+    const REQUEST_URL      = 'https://api.twitter.com/oauth/access_token';
+    const PROVIDER_TWITTER = 'api.twitter.com';
+    const API_KEY_TEST          = 'kurJalaArRFtwhnZCoMxB2kKU'; // コグニートに既に設定されていたKEY
+    const API_SECRET_TEST       = 'oOCDmf29DyJyfxOPAaj8tSASzSPAHNepvbxcfVLkA9dJw7inYa'; //
+    // const API_KEY_TEST          = '3rrbNV3OXeBjKlZV3NRQRNS0k'; // 自前で用意したKEY
+    // const API_SECRET_TEST       = 'LEblop9pEOemasvddlGuvMzpkKc6608TuIhTaxU4YtiCaE3VjE'; // 自前
+    // const API_KEY_PRODUCTION = '';
+    // const API_SECRET_PRODUCTION = '';
+
     /**
      * username password register
      * @param string POST $username
@@ -77,7 +86,7 @@ class Controller_V1_Register extends Controller_V1_Base
     }
 
     /**
-     * sns register
+     * sns(Facebook) register
      *
      * @param string POST $username
      * @param string POST $profile_img
@@ -95,23 +104,37 @@ class Controller_V1_Register extends Controller_V1_Base
         $sns_token   = Input::post('token');
         $provider    = Input::post('provider');
 
-        // provider facebookの時
-        /*
-        if ($provider === 'graph.facebook.com)') {
-            $username    = Input::post('username');
-            $profile_img = Input::post('profile_img');
-            $sns_token   = Input::post('token');
-            $provider    = Input::post('provider');
-        } else {
-            $username    = Input::post('username');
+        self::register_user($username, $user_id, $provider, $sns_token, $identity_id, $profile_img, $badge_num);
+    }
 
-            // サーバ側で保持していたtwitte_proifile_img / tokenを持ってくる
-            // $profile_img = get_
-            // $snn_token = get?
-            // $provider = "api.twitter";
-        }
-        */
+    /**
+     * sns(twitter) register
+     * @param String POST $username
+     */
+    public function action_twitter_sign_up()
+    {
+        $keyword  = "SNS登録";
+        $badge_num= 0;
+        $provider = "api.twitter";
+        $username = Input::post('username');
 
+        session_start();
+        // サーバ側で保持していたtwitte_proifile_img / tokenを持ってくる
+        $profile_img = $_SERVER['profile_img'];
+        $sns_token = $_SERVER['sns_token'];
+        self::register_user($username, $user_id, $provider, $sns_token, $identity_id, $profile_img, $badge_num);
+    }
+
+    public static function register_user(
+        $username,
+        $user_id,
+        $provider,
+        $sns_token,
+        $identity_id,
+        $profile_img,
+        $badge_num
+    )
+    {
         $this->post_check();
 
         try {
@@ -156,5 +179,285 @@ class Controller_V1_Register extends Controller_V1_Base
             error_log($e);
             exit;
         }
+    }
+
+    private static function get_twitter_data()
+    {
+        $API_KEY_TEST    = self::API_KEY_TEST;
+        $API_SECRET_TEST = self::API_SECRET_TEST;
+
+        // Callback URL
+        $Callback_url = ( !isset($_SERVER['HTTPS']) ||
+        empty($_SERVER['HTTPS']) ? 'http://' : 'https://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+        // 連携アプリを認証をクリックして帰ってきた時
+        if (isset( $_GET['oauth_token'] ) && !empty( $_GET['oauth_token'] ) ) {
+            session_start();
+            @$request_token_secret = $_SESSION['oauth_token_secret'];
+            $request_url = self::REQUEST_URL;
+            $request_method = 'POST';
+            $signature_key = rawurlencode($API_SECRET_TEST) . '&' . rawurlencode($request_token_secret);
+
+            $params = [
+                'oauth_consumer_key'    => $API_KEY_TEST,
+                'oauth_token'           => $_GET['oauth_token'],
+                'oauth_signature_method'=> 'HMAC-SHA1',
+                'oauth_timestamp'       => time(),
+                'oauth_verifier'        => $_GET['oauth_verifier'],
+                'oauth_nonce'           => microtime(),
+                'oauth_version'         => '1.0',
+            ];
+
+            foreach ($params as $key => $value)
+            {
+                $params[$Key] = rawurldecode($value);
+            }
+            // 連想配列をアルファベット順に並び替え
+            ksort($params);
+            // パラメータの連想配列を[キー=値&キー=値...]の文字列に変換
+            $request_params = http_build_query($params, '', '&');
+            // 変更した文字列をURLエンコードする
+            $request_params = rawurlencode($request_params);
+            // リクエストUメソッドをURLエンコードする
+            $encoded_request_method = rawurlencode($request_method);
+            // リクエストURLをURLエンコードする
+            $signature_data = $encoded_request_method . '&' . $encoded_request_method . '&' . $request_params;
+            // キー[$signature_key]とデータ[$signature_data]を利用して、HMAC-SHA1方式のハッシュ値に変換する
+            $hash = hash_hmac('sha1', $signature_data, $signature_key, TRUE);
+            // base64エンコードして、著名[$signature]が完成
+            $signature = base64_encode($hash);
+            // パラメータの連想配列、[$params]に、作成した著名を加える
+            $params['oauth_signature'] = $signature;
+            // パラメータの連想配列を[キー=値,キー=値,...]の文字列に変換する
+            $header_params = http_build_query($params, '', ',');
+            // リクエスト用のコンテキストを作成する
+            $context = [
+                'http' => [
+                    'method' => $request_method,
+                    'header' => [
+                        'Authorization: OAuth ' . $header_params,
+                    ],
+                ],
+            ];
+            // cURLでリクエスト
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $request_url);
+            curl_setopt($curl, CURLOPT_HEADER, 1);
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $context['http']['method']);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $context['http'['header']]);
+            curl_setopt($curl, CURLOPT_TIMEOUT, 5);
+            $res1 = curl_exec($curl);
+            $res2 = curl_getinfo($curl);
+            curl_close($curl);
+            $response = substr($res1, $res2['header_size']);
+            $header   = substr($res1, 0, $res2['header_size']);
+
+            if (!isset($response) || empty($response)) {
+                $error = 'リクエストが失敗しました。Twitterの応答自体ありません';
+                $data = [
+                    "error_msg" => $error,
+                ];
+                $base_data = self::base_template($api_code = "SUCCESS",
+                    $api_message = "Successful API request",
+                    $login_flag  = 1, $data, $jwt = ""
+                );
+                echo self::output_json($base_data);
+            } else {
+                // 文字列を[&]で区切る
+                $parameters = explode('&', $response);
+                // エラー判定
+                if ( !isset($parameters[1] ) || empty( $parameters[1])) {
+                    $error_msg = true;
+                } else {
+                    // それぞれの値を格納する配列
+                    $query = [];
+                    // [$parameters]をループ処理
+                    foreach ($parameters as $parameter)
+                    {
+                        // 文字列を[=]で区切る
+                        $pair = explode('=', $parameter);
+                        // 配列に格納
+                        if (isset($pair[1])) {
+                            $query[$pair[0]] = $pair[1];
+                        }
+                    }
+                    if ( isset($query['oauth_token']) || !isset($query['oauth_token_secret'])) {
+                        $error_msg = true;
+                    } else {
+                        $access_token = $query['oauth_token'];
+                        $access_token_secret = $query['oauth_token_secret'];
+                        $user_id = $query['user_id'];
+                        $screen_name = $query['screen_name'];
+                        $image = "http://www.paper-glasses.com/api/twipi/" . $screen_name;
+                    }
+                }
+                // エラーの場合
+                if ( isset($error_msg) && !empty($error_msg)) {
+                    $error = '';
+                    $error .= 'アクセストークンを取得できませんでした。セッションが上手く働いていない可能性があります';
+                    $data = [
+                        "error_msg" => $error_msg
+                    ];
+                    $base_data = self::base_template($api_code = "SUCCESS",
+                            $api_message = "Successful API request",
+                            $login_flag  = 1,
+                            $data,
+                            $jwt = ""
+                    );
+                    echo self::output_json($base_data);
+                }
+            }
+            // セッション終了
+            $_SESSION = [];
+            session_destroy();
+        } else if( isset($_GET['denied']) && !empty( $_GET['denied'])) {
+            // キャンセルクリックして返ってきた時、エラーメッセージを出力して終了
+            die('You have rejected the app');
+            exit;
+        } else {
+            $oauth_token = self::getRequestToken();
+            return $oauth_token;
+        }
+        if ( isset ($error) && $error) {
+            die($error);
+        }
+        if ( empty($data)) {
+            $data = [];
+        } else {
+            $access_token = $data['oauth_token'];
+        }
+        $token = $access_token . ";" . $query['oauth_token_secret'];
+        $data[0]['access_token']        = $access_token;
+        $data[0]['access_token_secret'] = $query['oauth_token_secret'];
+        $data[0]['token']               = $token;
+        $data[0]['image']               = $image;
+        return $data;
+    }
+
+    // Request Token
+    public static function getRequestToken()
+    {
+        $API_KEY_TEST    = self::API_KEY_TEST;
+        $API_SECRET_TEST = self::API_SECRET_TEST;
+        $callback_url = ( !isset($_SERVER['HTTPS']) || empty($_SERVER['HTTPS']) ? 'http://' : 'https://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ;
+        // リクエストトークンの取得
+        $access_token_secret = '' ;
+        // エンドポイントURL
+        $request_url = 'https://api.twitter.com/oauth/request_token' ;
+        // リクエストメソッド
+        $request_method = 'POST' ;
+        // キーを作成する (URLエンコードする)
+        $signature_key = rawurlencode( $API_SECRET_TEST ) . '&' . rawurlencode( $access_token_secret ) ;
+        // パラメータ([oauth_signature]を除く)を連想配列で指定
+        $params = [
+            'oauth_callback'         => $callback_url,
+            'oauth_consumer_key'     => $API_KEY_TEST,
+            'oauth_signature_method' => 'HMAC-SHA1',
+            'oauth_timestamp'        => time(),
+            'oauth_nonce'            => microtime(),
+            'oauth_version'          => '1.0',
+        ];
+        // 各パラメータをURLエンコード
+        foreach ($params as $key => $value) {
+            if ($key == 'oauth_callback') {
+                continue;
+            }
+            // URLエンコード処理
+            $params[$Key] = rawurlencode($value);
+        }
+        // 連想配列をアルファベット順に並び替える
+        ksort($params);
+
+        // パラメータの連想配列を[キー=値&キー=値...]の文字列に変換する
+        $request_params = http_build_query( $params , '' , '&' ) ;
+        // 変換した文字列をURLエンコードする
+        $request_params = rawurlencode( $request_params ) ;
+        // リクエストメソッドをURLエンコードする
+        $encoded_request_method = rawurlencode( $request_method ) ;
+        // リクエストURLをURLエンコードする
+        $encoded_request_url = rawurlencode( $request_url ) ;
+        // リクエストメソッド、リクエストURL、パラメータを[&]で繋ぐ
+        $signature_data = $encoded_request_method . '&' . $encoded_request_url . '&' . $request_params ;
+        // キー[$signature_key]とデータ[$signature_data]を利用して、HMAC-SHA1方式のハッシュ値に変換する
+        $hash = hash_hmac( 'sha1' , $signature_data , $signature_key , TRUE ) ;
+        // base64エンコードして、署名[$signature]が完成する
+        $signature = base64_encode( $hash ) ;
+        // パラメータの連想配列、[$params]に、作成した署名を加える
+        $params['oauth_signature'] = $signature ;
+        // パラメータの連想配列を[キー=値,キー=値,...]の文字列に変換する
+        $header_params = http_build_query( $params , '' , ',' ) ;
+        // リクエスト用のコンテキストを作成する
+        $content = [
+            'http' => [
+                'method' => $request_method,
+                'header' => [
+                    'Authorization: OAuth ' . $header_params,
+                ],
+            ],
+        ];
+        // cURLを使ってリクエスト
+        $curl = curl_init() ;
+        curl_setopt( $curl , CURLOPT_URL , $request_url ) ;
+        curl_setopt( $curl , CURLOPT_HEADER, 1 ) ; 
+        curl_setopt( $curl , CURLOPT_CUSTOMREQUEST , $context['http']['method'] );     // メソッド
+        curl_setopt( $curl , CURLOPT_SSL_VERIFYPEER , false );               // 証明書の検証を行わない
+        curl_setopt( $curl , CURLOPT_RETURNTRANSFER , true );                // curl_execの結果を文字列で返す
+        curl_setopt( $curl , CURLOPT_HTTPHEADER , $context['http']['header'] );      // ヘッダー
+        curl_setopt( $curl , CURLOPT_TIMEOUT , 5 );                    // タイムアウトの秒数
+        $res1 = curl_exec( $curl );
+        $res2 = curl_getinfo( $curl );
+        curl_close( $curl );
+
+        // 取得したデータ
+        $response = substr( $res1, $res2['header_size'] );
+        $header = substr( $res1, 0, $res2['header_size'] );
+
+        // リクエストが成功しなかった場合
+        if ( !isset($response) || empty($response)) {
+            $error = 'リクエストが失敗してしまったようです。Twitterかの応答自体ありません';
+        } else {
+            // 成功した場合
+            // 文字列を[&]で区切る
+            $parameters = explode( '&' , $response ) ;
+            if( !isset( $parameters[1] ) || empty( $parameters[1] ) ) {
+                $error_msg = true ;
+            } else {
+                // それぞれの値を格納する配列
+                $query = array() ;
+                // [$parameters]をループ処理
+                foreach( $parameters as $parameter )
+                {
+                    // 文字列を[=]で区切る
+                    $pair = explode( '=' , $parameter ) ;
+
+                    // 配列に格納する
+                    if( isset($pair[1]) ) {
+                      $query[ $pair[0] ] = $pair[1] ;
+                    }
+                }
+                // エラー判定
+                if( !isset( $query['oauth_token'] ) || !isset( $query['oauth_token_secret'] ) ) {
+                    $error_msg = true ;
+                } else {
+                    return $query['oauth_token'] .";" . $query['oauth_token_secret'];
+                    exit;
+                }
+            }
+            // エラーの場合
+            if( isset( $error_msg ) && !empty( $error_msg ) ) {
+                $error = '' ;
+                $error .= 'リクエストトークンを取得できませんでした。[$API_KEY_TEST]と[$callback_url]、そしてTwitterのアプリケーションに設定している[Callback URL]を確認して下さい。' ;
+                $error .= '([Callback URLに設定されているURL]→<mark>' . $callback_url . '</mark>)' ;
+                error_log($error);
+            }
+        }
+        // エラーメッセージがある場合
+        if ( isset($error) && $error) {
+            error_log($error);
+            exit;
+        }
+        return $query['oauth_token'];
     }
 }
